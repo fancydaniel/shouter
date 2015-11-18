8714b37e31d20f1cef64ad39c67257f39753ef38
Daniel Uribe <duribe8@gmail.com>
2015-11-17 22:27:12 -0800

    Added links to shout, user, added flash notice

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,9 +1,18 @@
 class ShoutsController < ApplicationController
 
+  def show
+    @shout = Shout.find(params[:id])
+
+  end
+
   def create
     shout = current_user.shouts.build(shout_parameters)
-    shout.save
-    redirect_to dashboard_path
+    if shout.save
+      redirect_to dashboard_path
+    else
+      flash.alert = "Could not shout."
+      redirect_to dashboard_path
+    end
   end
 
   private
