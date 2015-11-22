a9592b42d6b8c193412018be46be28380cff08a4
Daniel Uribe <duribe8@gmail.com>
2015-11-21 16:16:37 -0800

    Added ability to follow user via following_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
   has_many :shouts
+
+  has_many :following_relationships, foreign_key: :follower_id
+  # following_relationships
+  # follower_id followed_user_id
+  has_many :followed_users, through: :following_relationships
 end
